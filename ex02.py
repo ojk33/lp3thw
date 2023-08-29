d8b265b00207ad927f81b77406efe71da9355362
Owen <142924168+ojk33@users.noreply.github.com>
2023-08-29 13:43:32 -0400

    Delete ex02.py

--- ex02.py
+++
@@ -1,9 +0,0 @@
-# A Comment, this is so you can read your program later.
-# Anything after the # is ignored by python.
-
-print("I could have code like this.") # and the comment after is ignored
-
-# You can also use comments to "disable" or comment out code:
-# print("This won't run.")
-
-print("This will run.")
